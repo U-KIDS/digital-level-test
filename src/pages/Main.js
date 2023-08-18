ec5dcb228fa7f800fd951490a51f2ff04ba87171
MEGUMMY1 <cys10902@nate.com>
2023-08-18 18:01:32 +0900

    img 추가

---
+++ src/pages/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Style.css";
+import { useLocation, useNavigate } from "react-router-dom";
+import barLogo from '../../paran_logo.png';
+import Meeting from './MeetingList';
+
+function main(){
+    const navigate = useNavigate();
+
+    return(
+        <div className="Main-container">
+            
+        </div>
+    )
+}
+
+export default main;
