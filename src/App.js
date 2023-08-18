976e7d2649f678a2508296621bb9155bff725e30
MEGUMMY1 <cys10902@nate.com>
2023-08-18 22:24:03 +0900

    main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import Main from "./main/Main";
+import Test from './test/Test';
+import Result from "./result/Result";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css"
+
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/test/:id' element={<Test />} />
+          <Route path='/result' element={<Result />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/main/Main.js
+++ src/main/Main.js
@@ -1,13 +1,22 @@
-import './main.css'
+import { Link } from 'react-router-dom';
+import './main.css';
+import startButton from '../images/StartButton.png';
 
 export default function Main() {
+    initSessionStorage();
+
     return (
-        <div className='main'>
-            <div className='title'>
-                <p>디지털</p>
-                <p>친화력</p>
-                <p>테스트</p>
-            </div>
+        <div className='main-container'>
+            <div className='main-title'/>
+            <div className='main-loading'/>
+            <Link to={`/test/1`}>
+                <img src={startButton} alt="startButton" className="main-start"></img>
+            </Link>
         </div>
     )
+}
+
+function initSessionStorage() {
+    sessionStorage.clear();
+    sessionStorage.setItem("level", 0);
 }
