cfb933f7fd920b5ffaedb111c018110ea5e2bd40
wwan13 <wwan13@naver.com>
2023-08-18 18:26:35 +0900

    feat main

---
+++ src/main/Main.js
@@ -0,0 +1,13 @@
+import './main.css'
+
+export default function Main() {
+    return (
+        <div className='main'>
+            <div className='title'>
+                <p>디지털</p>
+                <p>친화력</p>
+                <p>테스트</p>
+            </div>
+        </div>
+    )
+}
